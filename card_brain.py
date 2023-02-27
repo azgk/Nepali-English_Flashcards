6a7465387625c7283361fdeb881ffe563d3b7da0
azgk <76268212+azgk@users.noreply.github.com>
2023-02-27 16:39:34 -0500

    Added home button. Combined go_thru_stack.py into card_brain.py, tested the code and debugged.

--- card_brain.py
+++ card_brain.py
@@ -1,17 +1,18 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import font
+import random
+import pandas
 
 BACKGROUND_COLOR = "#BAD7E9"
 
 
 class CardBrain:
 
-  def __init__(self, shuffled_rank, new_dict):
-    self.shuffled_rank = shuffled_rank
-    self.new_dict = new_dict  # Output dict after remembered words are removed from the stack.
+  def __init__(self):
     self.flip_timer = lambda x: 0  # A trick to flip the cards.
     self.i = 0  # Use this i to loop through the shuffled_rank while running different button commands.
+    self.stack = None
 
     # Basic/static UI SETUP_________________________________________________________________
     self.window = Tk()
@@ -29,17 +30,27 @@ class CardBrain:
     self.welcome_text = self.canvas.create_text(440, 180, text="Welcome! Please choose a stack of flash cards.", fill=
                                                 "Black", font=("Arial", 30, "normal"))
     self.button_style = font.Font(family='Helvetica', size=18, weight='bold')
-    for i in range(1, 6):
-      setattr(CardBrain, f"btn_stack_{i}", Button(text=f"Cards {i * 20 - 19} - {i * 20}", command=self.front_card,
-                                                  highlightbackground=BACKGROUND_COLOR, font=self.button_style,
-                                                  borderwidth=0))
 
+    self.btn_stack_1 = Button(text=f"Cards 01 - 20", command=lambda: self.front_card("stack_1", True),
+                              highlightbackground=BACKGROUND_COLOR, font=self.button_style, borderwidth=0)
+    self.btn_stack_2 = Button(text=f"Cards 21 - 40", command=lambda: self.front_card("stack_2", True),
+                              highlightbackground=BACKGROUND_COLOR, font=self.button_style, borderwidth=0)
+    self.btn_stack_3 = Button(text=f"Cards 41 - 60", command=lambda: self.front_card("stack_3", True),
+                              highlightbackground=BACKGROUND_COLOR, font=self.button_style, borderwidth=0)
+    self.btn_stack_4 = Button(text=f"Cards 61 - 80", command=lambda: self.front_card("stack_4", True),
+                              highlightbackground=BACKGROUND_COLOR, font=self.button_style, borderwidth=0)
+    self.btn_stack_5 = Button(text=f"Cards 81 - 100", command=lambda: self.front_card("stack_5", True),
+                              highlightbackground=BACKGROUND_COLOR, font=self.button_style, borderwidth=0)
+
+    home_img = PhotoImage(file="images/home.png")
+    home_button = Button(image=home_img, command=self.home_page(), highlightthickness=0, bd=0)
+    home_button.grid(column=3, row=0, pady=20)
     info_img = PhotoImage(file="images/info.png")
     info_button = Button(image=info_img, command=self.click_info, highlightthickness=0, bd=0)
     info_button.grid(column=4, row=0, pady=20)
-    self.button_right = Button(text="I know this!", command=self.click_right, bd=0, highlightthickness=0,
+    self.button_remembered = Button(text="I know this!", command=self.click_remembered, bd=0, highlightthickness=0,
                           highlightbackground=BACKGROUND_COLOR, font=self.button_style, padx=20, pady=5, borderwidth=0)
-    self.button_wrong = Button(text="Need to review", command=self.click_right, bd=0, highlightthickness=0,
+    self.button_need_review = Button(text="Need to review", command=self.click_need_review, bd=0, highlightthickness=0,
                           highlightbackground=BACKGROUND_COLOR, font=self.button_style, padx=10, pady=5, borderwidth=0)
 
     self.btn_stack_1.grid(column=0, row=5)
@@ -47,8 +58,6 @@ class CardBrain:
     self.btn_stack_3.grid(column=2, row=5)
     self.btn_stack_4.grid(column=0, row=6)
     self.btn_stack_5.grid(column=1, row=6)
-
-    #TODO: need to add a btn for home page.
 
     self.window.mainloop()
 
@@ -59,48 +68,69 @@ class CardBrain:
 
   def hide_home_page(self):
     self.canvas.itemconfig(self.welcome_text, state="hidden")
-    for i in range(1, 6):
-      getattr(CardBrain, f"btn_stack_{i}").grid_forget()
+    self.btn_stack_1.grid_forget()
+    self.btn_stack_2.grid_forget()
+    self.btn_stack_3.grid_forget()
+    self.btn_stack_4.grid_forget()
+    self.btn_stack_5.grid_forget()
 
   # showing cards ___________________________________________________________________
-  def front_card(self):
-    # TODO: FLAG pass user choice (stack) to go_thru_stack (change names) to generate shuffle and something.
+  def front_card(self, stack, first_run=False):
+    self.stack = stack
+    df = pandas.read_csv(f"data/{stack}.csv")
+
+    if first_run:
+      # shuffled_rank is a list of indice (aka rank of usage frequency from original list).
+      self.shuffled_rank = df["Rank of Frequency"].tolist()
+      random.shuffle(self.shuffled_rank)
+      # use new_dict to save words NOT remembered (without messing with the stack of cards during the loop).
+      self.new_dict = {}
+      for index, row in df.iterrows():
+          self.new_dict[row["Rank of Frequency"]] = [row["Rank of Frequency"], row["Nepali"], row["English"]]
+
     rank = self.shuffled_rank[self.i]
     self.window.after_cancel(self.flip_timer)
     self.hide_home_page()
     self.canvas.itemconfig(self.canvas_image, image=self.card_front_img)
     self.canvas.itemconfig(self.card_title, text="Nepali", fill="Black")
     self.canvas.itemconfig(self.word_text, text=self.new_dict[rank][1], fill="Black")
-    self.button_wrong.grid(column=0, row=9)
-    self.button_right.grid(column=3, row=9)
+    self.button_need_review.grid(column=0, row=9)
+    self.button_remembered.grid(column=3, row=9)
     self.flip_timer = self.window.after(4000, self.back_card)
 
   def back_card(self):
-    rank = self.shuffled_rank[self.i]
-    self.canvas.itemconfig(self.canvas_image, image=self.card_back_img)
-    self.canvas.itemconfig(self.card_title, text="English", fill="White")
-    self.canvas.itemconfig(self.word_text, text=self.new_dict[rank][2], fill="White")
-    self.hide_home_page()
+    try:
+      rank = self.shuffled_rank[self.i]
+      self.canvas.itemconfig(self.canvas_image, image=self.card_back_img)
+      self.canvas.itemconfig(self.card_title, text="English", fill="White")
+      self.canvas.itemconfig(self.word_text, text=self.new_dict[rank][2], fill="White")
+      self.hide_home_page()
+    except IndexError:
+      pass
 
   def end_of_cards(self):
     self.canvas.itemconfig(self.card_title, text="")
     self.canvas.itemconfig(self.word_text, text="Good job! You have gone through this stack.", font=("Arial", 24, "normal"))
+    new_df = pandas.DataFrame.from_dict(self.new_dict, orient='index',
+                                        columns=["Rank of Frequency", "Nepali", "English"])
+    new_df.to_csv(f"data/{self.stack}.csv")
 
-  def click_right(self):
+  def click_remembered(self):
     rank = self.shuffled_rank[self.i]
     self.new_dict.pop(rank)
     self.i += 1
     if self.i == len(self.shuffled_rank):
       self.end_of_cards()
     else:
-      self.front_card()
+      self.front_card(self.stack)
 
-  def click_wrong(self):
+  def click_need_review(self):
+    print(self.new_dict)
     self.i += 1
     if self.i == len(self.shuffled_rank):
       self.end_of_cards()
     else:
-      self.front_card()
+      self.front_card(self.stack)
 
   def click_info(self):
       messagebox.showinfo(
--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 # References: 1000 most frequently used Nepali words from https://1000mostcommonwords.com/1000-most-common-nepali-words/?_ga=2.230450588.1648665285.1641086668-45386293.1641086668&_gl=1%2A1nafr1s%2A_ga%2ANDUzODYyOTMuMTY0MTA4NjY2OA..%2A_ga_8KVRFXKPM6%2AMTY0MTA4NjY2Ny4xLjAuMTY0MTA4NjY2Ny4w
 import pandas
 from make_card_stacks import CardStacks
-from go_thru_stack import GoThruStack
+from card_brain import CardBrain
 
 all_words_df = pandas.read_csv("data/original_100_common_words.csv")
-# card_stacks = CardStacks(all_words_df) # This line only need to be run once unless new words are added to the original list.
-go_thru_stack = GoThruStack("stack_1")
+# card_stacks = CardStacks(all_words_df) # This line only need to be run once unless new words are added to the
+# original list. Or, if you want to reset card stacks.
+card_brain = CardBrain()
 
 # TODO: add a word count. show it somewhere (total words, words remembered, words went through, words not remembered). save this in a separate cvs file (save review progress)
 # TODO: what is a better way of doing click_right() other than breaking up the loop to several functions?
-# TODO: update readme to add features.
+# TODO: update readme to add features etc. add a photo for nepal.
+# TODO: make a repl link.
+#  word 88 is wrong. add nepali word for picture. 
 
